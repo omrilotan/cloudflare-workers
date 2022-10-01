714f66ec6bfba4109a7473bf37eb73456e514703
omrilotan <omrilotan@hotmail.com>
2022-10-01 17:35:17 +0100

    [lib] log verify the token is not empty

--- lib/log/index.ts
+++ lib/log/index.ts
@@ -8,8 +8,8 @@ export async function log(record: Record, token: string): Promise<Response> {
 	if (typeof record !== "object") {
 		throw new TypeError("record must be an object");
 	}
-	if (typeof token !== "string") {
-		throw new TypeError("token must be a string");
+	if (typeof token !== "string" || token === "") {
+		throw new TypeError("token must be non empty a string");
 	}
 
 	const response = await fetch(
