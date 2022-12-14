1fa6de187038ae8a2b660e75e488bf00c1302ca2
omrilotan <omrilotan@hotmail.com>
2022-12-14 17:23:58 +0000

    add echo worker for debugging and experimenting on reqeusts coming into your zone

---
+++ services/echo/README.md
@@ -0,0 +1,3 @@
+# Echo
+
+Print some details from request. Used for debugging and experimenting on reqeusts coming into your zone.
---
+++ services/echo/src/index.ts
@@ -0,0 +1,56 @@
+type Env = {};
+
+/**
+ * Display a list of entries in HTML
+ */
+function displayEntries(entries: [string, string][], indentation = 1): string {
+	return entries
+		.map(([key, value]) =>
+			value
+				? `${Array.from({ length: indentation }).join("\t")}<b>${key}</b>: ${
+						typeof value === "object"
+							? `\n${displayEntries(Object.entries(value), indentation + 1)}`
+							: value
+				  }`
+				: ""
+		)
+		.filter(Boolean)
+		.join("\n");
+}
+
+const handler: ExportedHandler = {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+		ctx.passThroughOnException();
+
+		const content = `<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Request details</title>
+	</head>
+	<body>
+		<h1>Request details</h1>
+		<h2>Request</h2>
+		<pre>${request.method} ${request.url}</pre>
+		<h2>Headers</h2>
+		<pre>${displayEntries(Array.from(request.headers))}</pre>
+		<h2>CF</h2>
+		<pre>${displayEntries(Object.entries(request.cf))}</pre>
+		<h2>Body</h2>
+		<pre>${await request.text()}</pre>
+
+	</body>
+</html>`;
+
+		return new Response(content, {
+			status: 200,
+			headers: new Headers([["Content-Type", "text/html"]]),
+		});
+	},
+};
+
+export default handler;
