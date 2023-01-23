73def59c6dd3884c3a41c406ec13ef6d56cd293d
omrilotan <omrilotan@users.noreply.github.com>
2023-01-23 11:05:41 +0000

    😎 Autofix

--- services/tv/src/index.ts
+++ services/tv/src/index.ts
@@ -1,7 +1,7 @@
 type Env = {
 	TV_HOST: string;
 	TV_PORT: string;
-}
+};
 
 const handler: ExportedHandler = {
 	async fetch(
@@ -9,9 +9,8 @@ const handler: ExportedHandler = {
 		env: Env,
 		ctx: ExecutionContext
 	): Promise<Response> {
-
 		const url = new URL(request.url);
-		url.protocol = 'http';
+		url.protocol = "http";
 		url.host = env.TV_HOST;
 		url.port = env.TV_PORT;
 		return fetch(url.toString(), request);
