4553bebad29b891d72929f4fa5426fa88832e914
omrilotan <omrilotan@users.noreply.github.com>
2023-04-18 09:42:20 +0000

    😎 Autofix

--- services/traffic-logger/wrangler.tmpl.js
+++ services/traffic-logger/wrangler.tmpl.js
@@ -1,4 +1,4 @@
-export const template = ({Section}) => ({
+export const template = ({ Section }) => ({
 	name: "traffic-logger-local",
 	main: "src/app/index.ts",
 	compatibility_date: "2022-07-22",
@@ -9,49 +9,65 @@ export const template = ({Section}) => ({
 			routes: [
 				{ pattern: "omrilotan.com/*", zone_name: "omrilotan.com" },
 				{ pattern: "briefly.omrilotan.com/*", zone_name: "omrilotan.com" },
-				{ pattern: "do.omrilotan.com/*", zone_name: "omrilotan.com" }
+				{ pattern: "do.omrilotan.com/*", zone_name: "omrilotan.com" },
 			],
 			services: [
-				{ binding: "MAIN", service: "traffic-logger-main", environment: "production" },
-				{ binding: "CANARY", service: "traffic-logger-canary", environment: "production" }
+				{
+					binding: "MAIN",
+					service: "traffic-logger-main",
+					environment: "production",
+				},
+				{
+					binding: "CANARY",
+					service: "traffic-logger-canary",
+					environment: "production",
+				},
 			],
 			kv_namespaces: [
-				{ binding: "ROLLOUT", id: "309b38e6127b48eb86ff9a58e715ac15" }
+				{ binding: "ROLLOUT", id: "309b38e6127b48eb86ff9a58e715ac15" },
 			],
 			vars: Section({
 				ROLLOUT_KEY: "traffic-logger-canary",
-				ROLLOUT_HEADER: "force-rollout"
-			})
+				ROLLOUT_HEADER: "force-rollout",
+			}),
 		}),
-	canary: Section({
-		name: "traffic-logger-canary",
-		unsafe: {
-			bindings: [
-				Section({ type: "analytics_engine", name: "TRAFFIC_ANALYTICS", dataset: "TRAFFIC_ANALYTICS" })
-			]
-		},
-		vars: {
-			VARIATION: "canary",
-			SEND_ANALYTICS: true,
-			SEND_LOGS: true
-		}
-	}),
-	main: Section({
-		name: "traffic-logger-main",
-		routes: [
-			{ pattern: "*ci-cd.net/*", zone_name: "ci-cd.net" },
-			{ pattern: "*danisilas.com/*", zone_name: "danisilas.com" }
-		],
-		unsafe: {
-			bindings: [
-				{ type: "analytics_engine", name: "TRAFFIC_ANALYTICS", dataset: "TRAFFIC_ANALYTICS" }
-			]
-		},
-		vars: {
-			VARIATION: "main",
-			SEND_ANALYTICS: false,
-			SEND_LOGS: true
-		}
-	})
-},
+		canary: Section({
+			name: "traffic-logger-canary",
+			unsafe: {
+				bindings: [
+					Section({
+						type: "analytics_engine",
+						name: "TRAFFIC_ANALYTICS",
+						dataset: "TRAFFIC_ANALYTICS",
+					}),
+				],
+			},
+			vars: {
+				VARIATION: "canary",
+				SEND_ANALYTICS: true,
+				SEND_LOGS: true,
+			},
+		}),
+		main: Section({
+			name: "traffic-logger-main",
+			routes: [
+				{ pattern: "*ci-cd.net/*", zone_name: "ci-cd.net" },
+				{ pattern: "*danisilas.com/*", zone_name: "danisilas.com" },
+			],
+			unsafe: {
+				bindings: [
+					{
+						type: "analytics_engine",
+						name: "TRAFFIC_ANALYTICS",
+						dataset: "TRAFFIC_ANALYTICS",
+					},
+				],
+			},
+			vars: {
+				VARIATION: "main",
+				SEND_ANALYTICS: false,
+				SEND_LOGS: true,
+			},
+		}),
+	},
 });
